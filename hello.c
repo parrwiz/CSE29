4998b60f6b04c7d38d1cc90443726aecb39a125d
parrwiz <69708717+parrwiz@users.noreply.github.com>
2024-10-01 22:25:09 +0000

    lab Done Extra Practice

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  char hello[] = "Hello!"; 
+  char hello2[] = { 72, 101, 108, 108, 111, 33, 0 };
+
+  puts(hello);
+  puts(hello2);
+char edit[]={"This part is editted"};
+puts(edit);
+  return 0;
+}
